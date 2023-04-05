9038bcde9cc07376490aea6b5e3d106c9d541e32
PeyHunter <Nando.goat@gmail.com>
2023-04-05 16:43:50 +0200

    Working on updating the mouse with js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let mouseCursor = document.querySelector(".cursor");
+    let navLi = document.querySelectorAll(".link")
 
-window.onload = function () {
-let video = document.getElementById("video-background")
-let text = document.getElementById("text-container")
-console.log(text)
+    window.addEventListener(`mousemove`, cursor); 
+
+    function cursor(event) {
+        mouseCursor.style.top = event.pageY + `px`;
+        mouseCursor.style.left = event.pageX + `px`;
+    }
+
+navLi.forEach(link => {
+    link.addEventListener(`mouseleave`, () => {
+        mouseCursor.classList.remove("link-grow");
+        link.classList.remove("link-hover")
+        
+    })
+    link.addEventListener(`mouseover`, () => {
+        mouseCursor.classList.add("link-grow");
+        link.classList.add("link-hover")
+    })
 }
+    )
+
+});
